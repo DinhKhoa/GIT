576536f2ea56febea080f4f361c2ec4f9c174d99
END <khoadeptrai07@gmail.com>
2023-10-04 22:12:15 +0700

    Tinh tong so nguyen to

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@
         {
             // ham nham mang 1 chieu
             // nhap n la do dai` mang, va nhap n phan tu
-            Console.WriteLine("Nhap vao do dai n:");
+            Console.Write("Nhap vao do dai n: ");
             string input = Console.ReadLine();
             int number = int.Parse(input);
             List<int> arrays = new List<int>();
             // nhap n phan tu 
             for (int i = 0; i < number; i++)
             {
-                Console.WriteLine("Nhap phan tu thu {0}", i);
+                Console.Write("Nhap phan tu thu {0}: ", i);
                 int value = int.Parse(Console.ReadLine());
                 arrays.Add(value);
             }
@@ -26,7 +26,7 @@
                 Console.WriteLine("So thu {0} la: {1}",i , arrays[i]);
             }
             // pull/ merge request
-            Console.WriteLine("Tong la: " + TinhTong(arrays));
+            Console.Write("Tong la: " + TinhTongSoNguyenTo(arrays));
             // ham tinh tong
             // ham tinh tong so nguyen to trong mang
             // ham tinh tong so hoan hao
@@ -40,6 +40,28 @@
             for (int i = 0; i < arrays.Count; i++)
             {
                 sum -= -arrays[i];
+            }
+            return sum;
+        }
+
+        static public int TinhTongSoNguyenTo(List<int> arrays)
+        {
+            int sum = 0;   
+            for (int i =0; i < arrays.Count; i++)
+            {
+                // Kiem tra so nguyen to
+                int SoUoc = 0;
+                for (int j = 1; j <= arrays[i]; j++)
+                {
+                    if (arrays[i] % j == 0)
+                    {
+                        SoUoc += 1;
+                    }
+                } 
+                if (SoUoc == 2)
+                {
+                    sum += arrays[i];
+                }
             }
             return sum;
         }
